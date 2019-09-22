e3e8b46a436f08f3393352bcd099bfe095587aed
dragonying <624168796@qq.com>
2019-09-22 10:24:28 +0800

    'first'

---
+++ src/Cmps/CpTest/Tool.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/9/22
+ * Time: 10:18
+ */
+
+namespace Cmps\CpTest;
+
+
+class Tool
+{
+    public static function index(){
+        echo "Hello World ！";
+    }
+
+}
